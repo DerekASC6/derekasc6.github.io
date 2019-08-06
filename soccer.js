5e4834ecf1240173ea66dcf1cad6d4267c1c192f
DerekASC6 <derekcriollo5@gmail.com>
2019-08-06 15:48:05 -0400

    done

---
+++ soccer.js
@@ -0,0 +1,66 @@
+$("#boca").click(function() {
+$('html, body').animate({
+    scrollTop: $(".boca_vid").offset().top
+  }, 2000);
+
+
+});
+
+$("#nycfc").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".nycfc_vid").offset().top
+      }, 2000);
+    
+    
+    });
+    
+$("#ldu").click(function() {
+$('html, body').animate({
+    scrollTop: $(".ldu_vid").offset().top
+  }, 2000);
+
+
+});
+
+$("#tigres").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".tigres_vid").offset().top
+      }, 2000);
+    
+    
+    });
+
+
+$("#flamengo").click(function() {
+        $('html, body').animate({
+            scrollTop: $(".flamengo_vid").offset().top
+          }, 2000);
+        
+        
+        });
+
+$("#lagalaxy").click(function() {
+            $('html, body').animate({
+                scrollTop: $(".lagalaxy_vid").offset().top
+              }, 2000);
+            
+            
+            });
+
+$("#sounders").click(function() {
+                $('html, body').animate({
+                    scrollTop: $(".sounders_vid").offset().top
+                  }, 2000);
+                
+                
+                });
+
+ $("#redbulls").click(function() {
+                    $('html, body').animate({
+                        scrollTop: $(".redbulls_vid").offset().top
+                      }, 2000);
+                    
+                    
+                    });
+                    
+        
